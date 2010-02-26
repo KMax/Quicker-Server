cf9e7afc48cb4832aa63c6c40c4a5631ca7a39bf
KMax <kolchinmax@gmail.com>
2010-02-26 16:01:33 +0300

    Описал методы реализующие операции над заметкой:удалить,добавить и изменить

--- src/rest/NoteResource.java
+++ src/rest/NoteResource.java
@@ -1,17 +1,23 @@
 package rest;
 
+import java.net.URI;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 /**
- * REST Web Service
+ * REST Веб-сервис
  */
 
-@Path("note/{id}")
+@Path("/note/")
 public class NoteResource {
     @Context
     private UriInfo context;
@@ -21,14 +27,47 @@ public class NoteResource {
     }
 
 	/**
-	 * Retrieves representation of an instance of rest.NoteResource
+	 * Посылает заметку по заданному id
 	 * @param id
 	 * @return an instance of java.lang.String
 	 */
+	@Path("/{id}")
 	@GET
     @Produces("application/xml")
 	public String getNote(@PathParam("id") int id) {
 		return "<note id='"+id+"' ><title>Пробная заметка</title>" +
 				"<text>блая блая блая блая блая блая</text></note>";
+	}
+
+	/**
+	 * Delete a note by id.
+	 * @param id
+	 * @return an instance of javax.ws.rs.core.Response
+	 */
+	@Path("/{id}")
+	@DELETE
+	public Response deleteNote(@PathParam("id") int id){
+		return Response.noContent().build();
+	}
+
+	/**
+	 * Update a note by id.
+	 * @return an instance of javax.ws.rs.core.Response
+	 */
+	@Path("/{id}")
+	@PUT
+	@Consumes("application/xml")
+	public Response updateNote(){
+		return Response.noContent().build();
+	}
+
+	/**
+	 * Add new note.
+	 * @return an instance of javax.ws.rs.core.Response
+	 */
+	@POST
+	@Consumes("application/xml")
+	public Response addNote(){
+		return Response.noContent().build();
 	}
 }
