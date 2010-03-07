43492c73ff0c37711a957daefa32b0130c05eefc
KMax <kolchinmax@gmail.com>
2010-03-08 01:01:43 +0300

    Made refactoring too

--- src/Quicker/Server/db/NoteDatabase.java
+++ src/Quicker/Server/db/NoteDatabase.java
@@ -1,5 +1,6 @@
 package Quicker.Server.db;
 
+import Quicker.Server.db.Database;
 import com.xhive.core.interfaces.XhiveSessionIf;
 import com.xhive.query.interfaces.XhiveXQueryValueIf;
 import com.xhive.util.interfaces.IterableIterator;
@@ -10,43 +11,16 @@ import javax.ejb.Stateless;
  *
  */
 @Stateless
-public class NoteDatabase {
-	@EJB
-	private Database db;
+public class NoteDatabase extends Database{
 
-	public NoteDatabase(){}
+	public NoteDatabase(){
+		super();
+	}
 
 	
 	public String getNoteById(String user, int id){
 		String query = "doc('note')/feed/entry[id="+id+"]";
 		return executeXQuery(user, query);
-	}
-
-	private String executeXQuery(String user, String query) {
-		IterableIterator<? extends XhiveXQueryValueIf> i = null;
-		XhiveSessionIf session = db.getDriver().createSession();
-		session.connect(Database.userName, Database.userPass, Database.dbName);
-		session.setReadOnlyMode(true);
-		try{
-		session.begin();
-		i = session.getDatabase()
-				.getRoot().get(user).executeXQuery(query);
-		//session.commit();
-		}catch(Exception ex){
-			//session.rollback();
-			ex.printStackTrace();
-		}
-		return i.next().toString();
-	}
-
-	private void executeXQueryUpdate(String user, String query){
-		XhiveSessionIf session = db.getDriver().createSession();
-		session.connect(Database.userName, Database.userPass, Database.dbName);
-		session.setReadOnlyMode(false);
-		session.begin();
-		IterableIterator<? extends XhiveXQueryValueIf> i = session.getDatabase()
-				.getRoot().get(user).executeXQuery(query);
-		session.commit();
 	}
 
 	/**
