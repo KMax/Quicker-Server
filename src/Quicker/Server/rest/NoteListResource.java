5e25f9435ffddba286f9a55580c243c385bcddb4
KMax <kolchinmax@gmail.com>
2010-03-08 13:40:22 +0300

    Implemented authorization in class NoteListResourses

--- src/Quicker/Server/rest/NoteListResource.java
+++ src/Quicker/Server/rest/NoteListResource.java
@@ -2,21 +2,17 @@ package Quicker.Server.rest;
 
 import Quicker.Server.UserAuthorization;
 import Quicker.Server.db.NoteDatabase;
-import Quicker.Server.db.UserDatabase;
-import com.sun.jersey.api.core.HttpRequestContext;
-import javax.annotation.security.PermitAll;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
 
 /**
  * REST Web Service
@@ -48,11 +44,16 @@ public class NoteListResource {
 	 * @return an instance of java.lang.String
 	 */
 	@GET
-    @Produces("application/xml")
+    @Produces("application/atom+xml")
 	public Response getNoteList(@PathParam("user") String user) {
-		userAuth.Auth(hh);
-		String s = notedb.getNoteList(user);
-		Response r = Response.ok(s).build();
+		Response r =null;
+		try{
+			userAuth.Auth(hh,user);
+			String s = notedb.getNoteList(user);
+			r = Response.ok(s).build();
+		}catch(WebApplicationException wae){
+			r = wae.getResponse();
+		}
 		return r;
 	}
 }
